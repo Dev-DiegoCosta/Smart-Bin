7ed2e491e655058c98171ba42258a32ca2a57a98
Diego Costa <77362401+Dev-DiegoCosta@users.noreply.github.com>
2025-05-12 06:52:59 -0300

    Create data_store.py

---
+++ data_store.py
@@ -0,0 +1,16 @@
+#data_store.py
+
+usuarios = {}
+
+def cadastrar_usuario(cpf):
+    if cpf not in usuarios:
+        usuarios[cpf] = 0
+        return True
+    return False
+
+def adicionar_pontos(cpf, pontos):
+    if cpf in usuarios:
+        usuarios[cpf] += pontos
+
+def get_pontos(cpf):
+    return usuarios.get(cpf, 0)
