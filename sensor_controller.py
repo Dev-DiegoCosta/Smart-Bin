bd05a72e984076cd21d22ebbea96714ae5ccec01
Diego Costa <77362401+Dev-DiegoCosta@users.noreply.github.com>
2025-05-12 06:57:18 -0300

    Update sensor_controller.py
    sensor de movimento

--- sensor_controller.py
+++ sensor_controller.py
@@ -1,1 +1,17 @@
 
+# sensor_controller.py
+
+
+# sensor_controller.py
+import time
+
+def sensor_movimento_detectado():
+    # Simulação de movimento (para depois usar GPIO)
+    print("Sensor de movimento ativado.")
+    return True
+
+def peso_detectado():
+    # Simulação de peso (para depois usar sensor real)
+    peso = 1.2  # Em kg
+    print(f"Peso detectado: {peso} kg")
+    return peso
